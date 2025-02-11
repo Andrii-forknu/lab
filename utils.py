89b5b18a69ce7647df47f1a1d75f50a5e1a188f3
Andrii-forknu <andrii.velychko@knu.ua>
2025-02-11 13:00:03 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,13 +11,3 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
-
-number = int(input("Enter a number: "))
-if is_prime(number):
-    print(f"{number} is a prime number.")
-else:
-    print(f"{number} is not a prime number.")
-
-# Приклад використання
-num = 5
-print(f"Факторіал {num} =", factorial(num))
